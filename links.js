d2ff75b4afcd173d76c28c73dbd3af5994e1df4d
Jon <jonathanringenberg@gmail.com>
2022-02-14 09:36:33 -0600

    Update links.js

--- links.js
+++ links.js
@@ -1,3 +1,11 @@
 $('exOne').click(function(){
     window.location = "https://github.com/jwringenberg/car";
 });
+
+$('exTwo').click(function(){
+    window.location = "https://jwringenberg.github.io/Mind-Ladder/";
+});
+
+$('exThree').click(function(){
+    window.location = "https://jwringenberg.github.io/Mind-Ladder/";
+});
